512631e486b8f16e397297d6822158b141bf9d4d
Darkdini921 <chigrin.00@mail.ru>
2025-01-17 00:34:36 +0300

    second update

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,16 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if ( clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
+        } else if ( clientOS == 1 && clientDeviceYear < 2015 ){
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная ОС");
         }
 
         // Задача 3
 
         int year = 2021;
-        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)){
             System.out.println(year + " год является високосным.");
         } else {
             System.out.println(year + " год не является високосным.");
@@ -46,32 +48,22 @@ public class Main {
 
         char monthNumber = 12;
 
-        if (monthNumber > 12){
-            System.out.println("Ошибка.");
-        }else {
             switch (monthNumber) {
-                case 1:
-                case 2:
-                case 12:
+                case 1,2,12:
                     System.out.println("Сейчас зима!");
                     break;
-                case 3:
-                case 4:
-                case 5:
+                case 3,4,5:
                     System.out.println("Сейчас весна!");
                     break;
-                case 6:
-                case 7:
-                case 8:
+                case 6,7,8:
                     System.out.println("Сейчас лето!");
                     break;
-                case 9:
-                case 10:
-                case 11:
+                case 9,10,11:
                     System.out.println("Сейчас осень!");
                     break;
+                default:
+                    System.out.println("Ошибка.");
             }
             }
 
     }
-}
